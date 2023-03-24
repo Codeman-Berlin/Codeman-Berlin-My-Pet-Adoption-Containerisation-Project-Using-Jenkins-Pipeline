62388ad2324854f52611ba6e9b915a32f0b4e815
Alawaiye <olusegun.alawaiye@cloudhight.com>
2023-03-24 17:12:10 +0100

    created Jenkins-elb and Prod_elb main.tf, output.tf, variable.tf files and populated the security group's variable.tf file

--- personal_module/sg/output.tf
+++ personal_module/sg/output.tf
@@ -1,27 +1,27 @@
 # output "ansible-sg-id" {
-#   value = aws_security_group.PACEUD_Ansible_SG.id
+#   value = aws_security_group.Codeman_Ansible_SG.id
 # }
 
 # output "docker-sg-id" {
-#   value = aws_security_group.PACEUD_Docker_SG.id
+#   value = aws_security_group.Codeman_Docker_SG.id
 # }
 
 # output "jenkins-sg-id" {
-#   value = aws_security_group.PACEUD_Jenkins_SG.id
+#   value = aws_security_group.Codeman_Jenkins_SG.id
 # }
 
 # output "sonarqube-sg-id" {
-#   value = aws_security_group.PACEUD_Sonarqube_SG.id
+#   value = aws_security_group.Codeman_Sonarqube_SG.id
 # }
 
 # output "mysql-sg-id" {
-#   value = aws_security_group.PACEUD_DB_Backend_SG.id
+#   value = aws_security_group.Codeman_Backend_SG.id
 # }
 
 # output "alb-sg-id" {
-#   value = aws_security_group.PACEUD_ALB_SG.id
+#   value = aws_security_group.Codeman_ALB_SG.id
 # }
 
 # output "bastion-sg-id" {
-#   value = aws_security_group.PACEUD_bastion_SG.id
+#   value = aws_security_group.Codeman_bastion_SG.id
 # }
--- variable.tf
+++ variable.tf
@@ -1,129 +1,37 @@
+# VPC Name
 variable "vpc_name" {
   default                          = "Codeman-VPC"
 }
+# Name to be attached to all resources
 variable "name" {
   default                          = "Codeman"
 }
+# VPC Classless Inter-Domain Routing Block (cidr block)
 variable "vpc_cidr" {
   default                          = "10.0.0.0/16"
 }
-
+# Availability Zone 1
 variable "az1" {
   default            = "eu-west-2a"
 }
-
+# Availability Zone 2
 variable "az2" {
   default            = "eu-west-2b"
 }
-
+# Private Subnet cidr-block 1
 variable "prv-sn1" {
   default            = "10.0.1.0/24"
 }
-
+#Private Subnet cidr-block 2
 variable "prv-sn2" {
   default            = "10.0.2.0/24"
 }
-
+# Public Subnet cidr-block 1
 variable "pub-sn1" {
   default            = "10.0.3.0/24"
 }
-
+# Public Subnet cidr-block 2
 variable "pub-sn2" {
   default            = "10.0.4.0/24"
 }
 
-# variable "http_port" {
-#   default            = 80
-# }
-
-
-# variable "ssh_port" {
-#   default            = 22
-# }
-
-# variable "proxy_port1" {
-#   default            = 8080
-# }
-
-# variable "proxy_port2" {
-#   default            = 9000
-# }
-
-# variable "MySQL_port" {
-#   default            = 3306
-# }
-
-# variable "des_http" {
-#   default            = "This port allows http access"   
-# }
-
-# variable "des_ssh" {
-#   default            = "This port allows ssh access"
-# }
-
-
-# variable "proxy" {
-#   default            = "This port allows proxy access"
-# }
-
-# variable "prot" {
-#   default            = "tcp"
-# }
-
-# variable "all_access" {
-#   default            = "0.0.0.0/0"
-# }
-
-# variable "keyname" {
-#   default            = "paceud-kp2"
-# }
-
-# variable "ec2_name" {
-#   default            = "Test-ec2"
-# }
-
-# variable "ec2_ami" {
-#   default            = "ami-05c96317a6278cfaa"
-# }
-
-# variable "instancetype" {
-#   default            = "t3.medium"  
-#   }
-
-# variable "sonar-name" {
-#   default            = "sonar-sever"
-#   }
-# variable "docker_name" {
-#   default            = "docker_server"
-# }
-
-# #ASG Variables
-# variable "ami-name" {
-# default = "host_ami"
-# }
-# variable "target-instance" {
-# default = "docker_server"
-# }
-# variable "launch-configname" {
-# default = "host_ASG_LC"
-# }
-
-# variable "sg_name3" {
-# default = "                                                "
-# }
-
-# variable "asg-group-name" {
-# default = "pacaad_ASG"
-# }
-# variable "vpc-zone-identifier" {
-# default = ""
-# }
-# variable "target-group-arn" {
-# default = ""
-# }
-# variable "asg-policy" {
-# default = ""
-# }
-# variable "alb" {
-#   default = "pacaad-lb"
-# }
